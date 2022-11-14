dd4e46268819b65222c1a12a0c123acd7d857f92
Johan Herrera <johanherrera20000@gmail.com>
2022-11-14 11:54:58 -0500

    Change the url path on the landing page

--- public/js/landing.js
+++ public/js/landing.js
@@ -4,7 +4,6 @@ gsap.from(`.landing-animation`, {y: 50, opacity: 0, duration: 1, stagger: .6});
 const btnLanding = document.querySelector(`.btn`);
 
 btnLanding.addEventListener(`click`, () => {
-    window.location.replace(`http://localhost:3001/notes`);
-    // window.location.replace(`notes.html`);
+    window.location.replace(`/notes`);
 
 })
